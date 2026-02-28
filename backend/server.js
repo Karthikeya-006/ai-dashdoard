996e5a1d4268b41904fa1d451a3f63057119ced1
sowmya0307 <sowmyaatluri02@gmail.com>
2026-02-28 10:08:19 +0530

    fix railway port

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
@@ -9,7 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => console.log(`🚀 Server running on port ${port}`));
